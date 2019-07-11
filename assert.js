3f04c70756786e37abfe129af48b708bcd985bdd
James Hylins <james.hylins@johnlewis.co.uk>
2019-07-11 14:28:29 +0100

    improves structure of index.html page

--- assert.js
+++ assert.js
@@ -1,7 +1,7 @@
 var assert = {
   equal: function(actual, expected) {
     if (!(actual === expected)) {
-      console.log("    Fail!");
+      console.log("    Fail! Expected: " + expected + ", but got : " + actual);
     } else {
       console.log("    Pass");
     }
